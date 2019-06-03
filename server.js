64b02ce9fab486d772a7ec09b23e18056cfea7f8
Faran Navazi <faran.navazi@gmail.com>
2019-06-03 17:40:39 -0500

    server.js: -Dependencies -Config -Router -Listener DONE!

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const path = require('path');
-const express = require('express');
-
+const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
- 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+  console.log("App listening on PORT " + PORT);
+});
