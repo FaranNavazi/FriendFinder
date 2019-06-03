855dc055acd564e2d6f4ab1cfea613aebd8302b1
Faran Navazi <faran.navazi@gmail.com>
2019-06-03 17:31:20 -0500

    friends.js has been updated with some people. photo folder and some photos also has been added

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+let friends = [
+    {
+        "name":"Jenny",
+        "photo":"../photos/img-1.jpg",
+        "scores":[1,2,3,4,5,1,2,4,3,5]
+    },
+    {
+        "name":"April",
+        "photo":"../photos/img-2.jpg",
+        "scores":[2,4,5,1,3,2,4,5,1,2]
+    },
+    {
+        "name":"Sue",
+        "photo":"../photos/img-3.jpg",
+        "scores":[3,2,4,5,1,2,4,5,1,3]
+    },
+    {
+        "name":"John",
+        "photo":"../photos/img-4.jpg",
+        "scores":[1,2,3,4,5,1,2,4,3,5]
+    },
+    {
+        "name":"Ali",
+        "photo":"../photos/img-5.jpg",
+        "scores":[5,1,2,3,4,1,2,5,1,4]
+    },
+    {
+        "name":"Robert",
+        "photo":"../photos/img-6.jpg",
+        "scores":[4,1,2,5,4,3,2,5,4,2]
+    }
+];
+
+module.exports = friends;
